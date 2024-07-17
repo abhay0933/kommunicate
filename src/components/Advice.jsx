6247d8f392c9c75806bf2712464a133c40f98226
abhay0933 <abhay80912@gmail.com>
2024-07-17 18:31:04 +0530

    first commit

---
+++ src/components/Advice.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import AdviceCounter from "./AdviceCounter";
+
+function Advice(props) {
+
+    let [adviceID, setAdviceID] = useState(null);
+    let [advice, setAdvice] = useState(null);
+
+    useEffect(() => {
+        setAdvice(null);
+        fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then((adviceData) => {
+
+            setAdvice(adviceData.slip.advice);
+            setAdviceID(adviceData.slip.id);
+
+        })
+        .catch(error => console.log("Error:", error))
+    }, [props.count]);
+
+
+    return (
+        <>
+            <div id="advice">
+                <AdviceCounter adviceID={adviceID} />
+                <p>&ldquo;{advice}&rdquo;</p>
+            </div>
+        </>
+    )
+}
+
+export default Advice;
